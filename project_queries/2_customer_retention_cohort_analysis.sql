f13f2f6d77184ddfb10e78499afced177803d6ef
Daniel Haslam <daniel.haslam@student.manchester.ac.uk>
2025-08-04 19:08:45 +0100

    Added cohort retention code

---
+++ project_queries/2_customer_retention_cohort_analysis.sql
@@ -0,0 +1,44 @@
+WITH customer_status AS (
+WITH customer_order_counter AS (
+SELECT
+	customerkey,
+	full_name,
+	orderdate AS prev_purchase_date,
+	
+	ROW_NUMBER() OVER (
+		PARTITION BY customerkey
+		ORDER BY orderdate DESC
+	) AS order_count,
+	
+	first_purchase_date
+FROM
+	order_customer_data
+)
+
+SELECT
+	customerkey,
+	full_name,
+	EXTRACT(YEAR FROM first_purchase_date) AS cohort_year,
+	prev_purchase_date,
+	CASE
+		WHEN prev_purchase_date < (SELECT MAX(orderdate) FROM sales)  - INTERVAL '6 months' THEN 'Churned' 
+		ELSE 'Active'
+	END AS status
+FROM
+	customer_order_counter 
+WHERE
+	order_count = 1 AND
+	first_purchase_date < (SELECT MAX(orderdate) FROM sales)  - INTERVAL '6 months'
+ORDER BY first_purchase_date DESC
+)
+
+SELECT
+	cohort_year,
+	status,
+	COUNT(*) AS num_customers,
+	SUM(COUNT(*)) OVER (PARTITION BY cohort_year) AS total_customers, 
+	ROUND(100 * COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY cohort_year), 2) AS percentage
+FROM
+	customer_status
+GROUP BY
+	cohort_year, status;
