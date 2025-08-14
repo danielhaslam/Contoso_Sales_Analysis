b309cbff6931af09d0f38353bb04a640ec7684af
Daniel Haslam <daniel.haslam@student.manchester.ac.uk>
2025-08-14 03:32:34 +0100

    Changed query to categorise medians by way of category, as opposed to year

--- project_queries/3_category_revenue_analysis.sql
+++ project_queries/3_category_revenue_analysis.sql
@@ -1,57 +1,54 @@
-WITH median_values AS
-(
+WITH median_values AS (
   SELECT
-    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (CASE
-                                                 WHEN ptd.orderdate::DATE BETWEEN '2021-01-01' AND '2021-12-31' THEN ptd.transaction_revenue END)) AS median_2021,
-    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (CASE
-                                                 WHEN ptd.orderdate::DATE BETWEEN '2022-01-01' AND '2022-12-31' THEN ptd.transaction_revenue END)) AS median_2022,
-    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (CASE
-                                                 WHEN ptd.orderdate::DATE BETWEEN '2023-01-01' AND '2023-12-31' THEN ptd.transaction_revenue END)) AS median_2023
-  FROM
-    product_transaction_data ptd
+    ptd.categoryname,
+    EXTRACT(YEAR FROM ptd.orderdate) AS order_year,
+    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY ptd.transaction_revenue) AS median_rev
+  FROM product_transaction_data ptd
+  WHERE ptd.orderdate::DATE BETWEEN '2021-01-01' AND '2023-12-31'
+  GROUP BY ptd.categoryname, EXTRACT(YEAR FROM ptd.orderdate)
 )
 
 SELECT
   ptd.categoryname AS category,
-
+  
   SUM(CASE
-      WHEN (ptd.orderdate::DATE BETWEEN '2021-01-01' AND '2021-12-31')
-      	  AND ptd.transaction_revenue > mv.median_2021
+      WHEN EXTRACT(YEAR FROM ptd.orderdate) = 2021
+           AND ptd.transaction_revenue > mv.median_rev
       THEN ptd.transaction_revenue
   END) AS upper_rev_2021,
 
   SUM(CASE
-      WHEN (ptd.orderdate::DATE BETWEEN '2021-01-01' AND '2021-12-31')
-          AND ptd.transaction_revenue <= mv.median_2021
+      WHEN EXTRACT(YEAR FROM ptd.orderdate) = 2021
+           AND ptd.transaction_revenue <= mv.median_rev
       THEN ptd.transaction_revenue
   END) AS lower_rev_2021,
 
   SUM(CASE
-      WHEN (ptd.orderdate::DATE BETWEEN '2022-01-01' AND '2022-12-31')
-          AND ptd.transaction_revenue > mv.median_2022
+      WHEN EXTRACT(YEAR FROM ptd.orderdate) = 2022
+           AND ptd.transaction_revenue > mv.median_rev
       THEN ptd.transaction_revenue
   END) AS upper_rev_2022,
 
   SUM(CASE
-      WHEN (ptd.orderdate::DATE BETWEEN '2022-01-01' AND '2022-12-31')
-          AND ptd.transaction_revenue <= mv.median_2022
+      WHEN EXTRACT(YEAR FROM ptd.orderdate) = 2022
+           AND ptd.transaction_revenue <= mv.median_rev
       THEN ptd.transaction_revenue
   END) AS lower_rev_2022,
 
   SUM(CASE
-      WHEN (ptd.orderdate::DATE BETWEEN '2023-01-01' AND '2023-12-31')
-          AND ptd.transaction_revenue > mv.median_2023
+      WHEN EXTRACT(YEAR FROM ptd.orderdate) = 2023
+           AND ptd.transaction_revenue > mv.median_rev
       THEN ptd.transaction_revenue
   END) AS upper_rev_2023,
 
   SUM(CASE
-      WHEN (ptd.orderdate::DATE BETWEEN '2023-01-01' AND '2023-12-31')
-          AND ptd.transaction_revenue < mv.median_2023
+      WHEN EXTRACT(YEAR FROM ptd.orderdate) = 2023
+           AND ptd.transaction_revenue <= mv.median_rev
       THEN ptd.transaction_revenue
   END) AS lower_rev_2023
 
-FROM
-  product_transaction_data ptd, 
-  median_values mv
-GROUP BY
-  ptd.categoryname
+FROM product_transaction_data ptd
+INNER JOIN median_values mv
+  ON mv.categoryname = ptd.categoryname AND mv.order_year = EXTRACT(YEAR FROM ptd.orderdate)
+GROUP BY ptd.categoryname
+ORDER BY ptd.categoryname;
